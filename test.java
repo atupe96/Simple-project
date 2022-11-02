dade9b7dcf8a0629597ed223e4e02321b4db436a
Arjun <atupe96@gmial.com>
2022-11-02 13:18:29 +0530

    Add

--- test.java
+++ test.java
@@ -1,5 +1,15 @@
+<<<<<<< Updated upstream
 public class Test{
 
 public void add(){
 
 System.out.println("Add Methode");
+=======
+public class Demo{
+ public void main (String [args]){
+int a=19;
+int b=21;
+int c=a+b;
+
+System.out.println(c);
+>>>>>>> Stashed changes
